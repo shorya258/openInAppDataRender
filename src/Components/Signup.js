73b734f0e34271e01e1a2aaa863d2c897a9339b3
Shorya <shoryahayaran@gmail.com>
2023-10-13 17:16:49 +0530

    updated placeholders in signup

--- src/Components/Signup.js
+++ src/Components/Signup.js
@@ -51,7 +51,7 @@ function Signup(props) {
                   onChange={onChange}
                   value={credentials.name}
                   className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
-                  placeholder="abc@xyz.com"
+                  placeholder="Full Name"
                 />
               </div>
             </div>
@@ -73,7 +73,7 @@ function Signup(props) {
                   onChange={onChange}
                   value={credentials.email}
                   className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
-                  placeholder="abc@xyz.com"
+                  placeholder="email@abc.com"
                 />
               </div>
             </div>
