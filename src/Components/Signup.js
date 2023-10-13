2fe95d486164fa7a8918dab6dc9879f8d7cac6d4
Shorya <shoryahayaran@gmail.com>
2023-10-13 17:14:32 +0530

    added signup page in front end

--- src/Components/Signup.js
+++ src/Components/Signup.js
@@ -1,9 +1,114 @@
+import { useState } from "react";
 import React from "react";
 
 function Signup(props) {
+  const [credentials, setCredentials] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const response = await fetch("http://localhost:5000/api/signup", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        name: credentials.name,
+        email: credentials.email,
+        password: credentials.password,
+      }),
+    });
+    const json = await response.json();
+    console.log("DONE", json);
+    if (!json.success) {
+      alert("enter valid credentials");
+    }
+  };
+  const onChange = (e) => {
+    setCredentials({ ...credentials, [e.target.name]: e.target.value });
+  };
   return (
     <div>
-      Signup
+      <h1 className="font-bold text-2xl">Sign Up</h1>
+      <p>Create an account</p>
+      <form>
+        <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+          {/* FULL NAME */}
+          <div className="sm:col-span-4">
+            <label
+              htmlFor="name"
+              className="block text-sm font-medium leading-6 text-gray-900"
+            >
+              Full Name
+            </label>
+            <div className="mt-2">
+              <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-black-600 sm:max-w-md">
+                <input
+                  type="text"
+                  name="name"
+                  onChange={onChange}
+                  value={credentials.name}
+                  className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+                  placeholder="abc@xyz.com"
+                />
+              </div>
+            </div>
+          </div>
+
+          {/* EMAIL */}
+          <div className="sm:col-span-4">
+            <label
+              htmlFor="email"
+              className="block text-sm font-medium leading-6 text-gray-900"
+            >
+              Email
+            </label>
+            <div className="mt-2">
+              <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-black-600 sm:max-w-md">
+                <input
+                  type="text"
+                  name="email"
+                  onChange={onChange}
+                  value={credentials.email}
+                  className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+                  placeholder="abc@xyz.com"
+                />
+              </div>
+            </div>
+          </div>
+          {/* PASSWORD */}
+          <div className="sm:col-span-4">
+            <label
+              htmlFor="password"
+              className="block text-sm font-medium leading-6 text-gray-900"
+            >
+              Password
+            </label>
+            <div className="mt-2">
+              <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-black-600 sm:max-w-md">
+                <input
+                  type="password"
+                  name="password"
+                  onChange={onChange}
+                  value={credentials.password}
+                  autoComplete="password"
+                  className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+                  placeholder="password"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+        <button
+          type="submit"
+          className="rounded-md bg-black px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-black focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black mt-2"
+          onClick={handleSubmit}
+        >
+          Submit
+        </button>
+      </form>
       <button className="" onClick={props.handleIsLoginChange}>
         New user?
       </button>
