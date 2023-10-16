36aa0129739e301b63ba743c1befc6c7a9198588
Shorya Hayaran <Shoryahayaran@gmail.com>
2023-10-16 12:26:42 +0530

    apple icons

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,15 +1,19 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./login.css";
 import { Link } from "react-router-dom";
 import GLogin from "./GLogin";
 import { useNavigate } from "react-router-dom";
-import appleLogIn from "./assets/appleSignIn.svg";
 function Login(props) {
   let navigate = useNavigate();
   const [credentials, setCredentials] = useState({
     email: "",
     password: "",
   });
+
+  useEffect(() => {
+    appleLogin();
+  }, []);
+
   const handleSubmit = async (e) => {
     e.preventDefault();
     const response = await fetch("http://localhost:5000/api/login", {
@@ -35,6 +39,25 @@ function Login(props) {
       navigate("/dashboard");
     }
   };
+  const appleLogin = () => {
+    var doc = new DOMParser().parseFromString(
+      `<svg width="18"
+      height="18"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M7.53348 1.52879C8.55093 0.197925 9.96544 0.191437 9.96544 0.191437C9.96544 0.191437 10.1758 1.44268 9.16505 2.64802C8.08578 3.93506 6.85904 3.72446 6.85904 3.72446C6.85904 3.72446 6.62869 2.71225 7.53348 1.52879ZM6.98845 4.60095C7.51188 4.60095 8.48334 3.8869 9.74783 3.8869C11.9244 3.8869 12.7807 5.42395 12.7807 5.42395C12.7807 5.42395 11.106 6.2737 11.106 8.33559C11.106 10.6616 13.1922 11.4632 13.1922 11.4632C13.1922 11.4632 11.7339 15.5368 9.76404 15.5368C8.8593 15.5368 8.15592 14.9317 7.20264 14.9317C6.23118 14.9317 5.26715 15.5594 4.63927 15.5594C2.84051 15.5594 0.568054 11.6952 0.568054 8.58896C0.568054 5.53288 2.49154 3.92971 4.29569 3.92971C5.46855 3.92971 6.3787 4.60095 6.98845 4.60095Z"
+        fill="#999999"
+      />
+    </svg>`,
+      "application/xml"
+    );
+    const btn =
+      document.querySelector(".g-sign-in-wrapper").children[1].children[0];
+    btn.innerHTML = "";
+    btn.appendChild(btn.ownerDocument.importNode(doc.documentElement, true));
+  };
   const onChange = (e) => {
     setCredentials({ ...credentials, [e.target.name]: e.target.value });
   };
@@ -47,9 +70,7 @@ function Login(props) {
       <div className="sign-in-content-wrapper">
         <div className="g-sign-in-wrapper">
           <GLogin />
-          <div className="apple-login">
-            <img src={appleLogIn} alt="apple log in button" />
-          </div>
+          <GLogin />
         </div>
 
         <div>
