984cc1d82dba32a6f1acc5a37d57b656b7c8d144
Shorya Hayaran <Shoryahayaran@gmail.com>
2023-10-16 15:39:13 +0530

    changed import to small case

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const User = require("../Models/user");
+const User = require("../models/user");
 const { body, validationResult } = require("express-validator");
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
