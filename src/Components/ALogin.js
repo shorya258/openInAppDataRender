0ca1a515a4e074441ce835737d4c38b79bd8ceae
Shorya <shoryahayaran@gmail.com>
2023-10-18 10:55:29 +0530

    code refactoring

--- src/Components/ALogin.js
+++ src/Components/ALogin.js
@@ -2,7 +2,7 @@ import React from "react";
 import AppleLogin from "react-apple-login";
 function ALogin() {
   return (
-    <div>
+    <div id="appleid-signin">
       <AppleLogin
         clientId="com.react.apple.login"
         redirectURI="https://redirectUrl.com"
